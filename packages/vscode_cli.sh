7e4461e544f6e13a3557b4bb89f762efc560c6d4
Rohit Rawat <rohitrawat@microsoft.com>
2024-04-02 09:32:58 +0530

    fix unexpected token

--- packages/vscode_cli.sh
+++ packages/vscode_cli.sh
@@ -1,4 +1,4 @@
-vscode_cli{
+vscode_cli(){
     vscode_url="https://vscode.download.prss.microsoft.com/dbazure/download/stable/863d2581ecda6849923a2118d93a088b0745d9d6/vscode_cli_alpine_x64_cli.tar.gz"
     curl -OL $vscode_url && tar -xvf vscode_cli_alpine_x64_cli.tar.gz && sudo mv code /usr/local/bin/
     rm vscode_cli_alpine_x64_cli.tar.gz
